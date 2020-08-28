d353b45aed6f5b6c6ce51fcf7d770e0c415a0b9e
Rodrigo Pimpao <rodrigo.pimpao94@gmail.com>
2020-08-27 23:24:14 -0300

    Use a FlatList to render all repos from the backend.

--- src/App.js
+++ src/App.js
@@ -73,38 +73,11 @@ export default function App() {
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
         <View style={styles.repositoryContainer}>
-          {repositories.map(repo => {
-            return (
-              <>
-                <Text style={styles.repository}>{repo.title}</Text>
-
-                <View style={styles.techsContainer}>
-                  {repo.techs.map(tech => {
-                    return (<Text key={tech} style={styles.tech}>{tech}</Text>);
-                  })}
-                </View>
-
-                <View style={styles.likesContainer}>
-                  <Text
-                    style={styles.likeText}
-                    // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-                    testID={`repository-likes-${repo.id}`}
-                  >
-                    {`${repo.likes} curtidas`}
-                  </Text>
-                </View>
-
-                <TouchableOpacity
-                  style={styles.button}
-                  onPress={() => handleLikeRepository(repo.id)}
-                  // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-                  testID={`like-button-${repo.id}`}
-                >
-                  <Text style={styles.buttonText}>Curtir</Text>
-                </TouchableOpacity>
-              </>
-            );
-          })}
+          <FlatList
+            data={repositories}
+            keyExtractor={repo => repo.id}
+            renderItem={({ item: repo }) => renderRepo(repo)}
+          />
         </View>
       </SafeAreaView>
     </>
